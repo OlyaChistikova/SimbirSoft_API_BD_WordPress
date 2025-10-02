790aa42170dff5a440432896c92f05e7b5062409
Olya Chistikova <olya.chistikova@mail.ru>
2025-10-02 14:08:59 +0300

    Removed method to check error existence of post and added data for user's tests on BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,22 +1,17 @@
 package tests;
 
 import helpers.BaseRequests;
-import helpers.DataBaseHelper;
 import helpers.ParametersProvider;
 import io.restassured.response.ResponseBodyExtractionOptions;
 import org.testng.Assert;
 
 import java.util.List;
 
+import static helpers.PostRepository.getPostById;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class BaseTest {
-
-    /**
-     * Экземпляр помощника для взаимодействия с базой данных.
-     */
-    private final DataBaseHelper dbHelper = new DataBaseHelper();
 
     /**
      * Токен авторизации для доступа к API.
@@ -29,12 +24,20 @@ public class BaseTest {
     public static final String POSTS_PATH = BaseRequests.POSTS_PATH;
 
     /**
+     * Путь для взаимодействия с пользователями.
+     */
+    public static final String USERS_PATH = BaseRequests.USERS_PATH;
+
+    /**
      * Данные для авторизации
      */
     protected static final String usernameAdmin = ParametersProvider.getProperty("usernameAdmin");
     protected static final String passwordAdmin = ParametersProvider.getProperty("passwordAdmin");
     protected static final String usernameAuthor = ParametersProvider.getProperty("usernameAuthor");
     protected static final String passwordAuthor = ParametersProvider.getProperty("passwordAuthor");
+    protected static final String usernameTest = ParametersProvider.getProperty("usernameTest");
+    protected static final String emailTest = ParametersProvider.getProperty("emailTest");
+    protected static final String passwordTest = ParametersProvider.getProperty("passwordTest");
 
     /**
      * Получение списка объектов любого типа по указанному маршруту без авторизации.
@@ -103,20 +106,11 @@ public class BaseTest {
      * @param content Ожидаемое содержание.
      * @param status  Ожидаемый статус.
      */
-    public void checkSuccessPostDb(Integer post_id, String title, String content, String status, DataBaseHelper dbHelper) {
-        Assert.assertEquals(dbHelper.getPostById(post_id).getId(), post_id, "ID поста в базе не совпадает");
-        Assert.assertEquals(dbHelper.getPostById(post_id).getTitle().getRaw(), title, "Заголовок поста в базе не совпадает");
-        Assert.assertEquals(dbHelper.getPostById(post_id).getContent().getRaw(), content, "Содержимое поста в базе не совпадает");
-        Assert.assertEquals(dbHelper.getPostById(post_id).getStatus(), status, "Статус поста в базе не совпадает");
-    }
-
-    /**
-     * Проверяет, что пост отсутствует в базе.
-     *
-     * @param post_id ID поста.
-     */
-    public void checkErrorDb(Integer post_id) {
-        Assert.assertNull(dbHelper.getPostById(post_id), "Пост найден в базе");
+    public void checkSuccessPostDb(Integer post_id, String title, String content, String status) {
+        Assert.assertEquals(getPostById(post_id).getId(), post_id, "ID поста в базе не совпадает");
+        Assert.assertEquals(getPostById(post_id).getTitle().getRaw(), title, "Заголовок поста в базе не совпадает");
+        Assert.assertEquals(getPostById(post_id).getContent().getRaw(), content, "Содержимое поста в базе не совпадает");
+        Assert.assertEquals(getPostById(post_id).getStatus(), status, "Статус поста в базе не совпадает");
     }
 
     /**
@@ -126,7 +120,7 @@ public class BaseTest {
      * @param status  Ожидаемый статус.
      */
     public void checkDeleteDb(Integer post_id, String status) {
-        Assert.assertEquals(dbHelper.getPostById(post_id).getId(), post_id, "Пост не найден в базе");
-        Assert.assertEquals(dbHelper.getPostById(post_id).getStatus(), status, "Статус поста в базе не совпадает");
+        Assert.assertEquals(getPostById(post_id).getId(), post_id, "Пост не найден в базе");
+        Assert.assertEquals(getPostById(post_id).getStatus(), status, "Статус поста в базе не совпадает");
     }
 }

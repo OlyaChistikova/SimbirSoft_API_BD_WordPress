fcad3c7ec7f344afcebf72ef110bcdb67dba9a32
Olya Chistikova <olya.chistikova@mail.ru>
2025-10-02 14:15:45 +0300

    Implemented methods for connecting to the database and executing queries to DataBaseHelper

--- src/test/java/helpers/DataBaseHelper.java
+++ src/test/java/helpers/DataBaseHelper.java
@@ -1,10 +1,6 @@
 package helpers;
 
-import pojo.DataPost;
-
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DataBaseHelper {
 
@@ -22,7 +18,7 @@ public class DataBaseHelper {
      * @param sql    SQL-запрос на обновление
      * @param params Массив параметров для заполнения в запросе
      */
-    private void executeUpdate(String sql, Object... params) {
+    static void executeUpdate(String sql, Object... params) {
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD); // Получаем соединение из пула
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             for (int i = 0; i < params.length; i++) {
@@ -44,7 +40,7 @@ public class DataBaseHelper {
      * @param <T>       Тип возвращаемых данных
      * @return Результат выполнения запроса
      */
-    private <T> T executeQuery(String sql, RowMapper<T> rowMapper, Object... params) {
+    static <T> T executeQuery(String sql, RowMapper<T> rowMapper, Object... params) {
         T result = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -68,53 +64,5 @@ public class DataBaseHelper {
     @FunctionalInterface
     public interface RowMapper<T> {
         T map(ResultSet rs) throws SQLException;
-    }
-
-    /**
-     * Метод для добавления нового поста в базу данных.
-     *
-     * @param post   Экземпляр класса DataPost, содержащий данные поста
-     * @param author ID автора поста
-     */
-    public void addPost(DataPost post, int author) {
-        String sql = "INSERT INTO wp_posts (post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_content_filtered, post_parent, guid, menu_order, post_type, post_mime_type, comment_count) VALUES (?, NOW(), NOW(), ?, ?, '', ?, 'open', 'open', '', '', '', '', NOW(), NOW(), '', 0, '', 0, 'post', '', 0)";
-        executeUpdate(sql, author, post.getContent().getRendered(), post.getTitle().getRaw(), post.getStatus());
-    }
-
-
-    /**
-     * Метод для получения всех постов из базы данных.
-     *
-     * @return Список объектов DataPost, содержащих все доступные посты
-     */
-    public List<DataPost> getAllPosts() {
-        String sql = "SELECT ID, post_title, post_content, post_status FROM wp_posts";
-        return executeQuery(sql, rs -> {
-            List<DataPost> posts = new ArrayList<>();
-            while (rs.next()) {
-                DataPost.Title titleObj = new DataPost.Title(rs.getString("post_title"), "");
-                DataPost.Content contentObj = new DataPost.Content(rs.getString("post_content"), "");
-                posts.add(new DataPost(rs.getInt("ID"), titleObj, contentObj, rs.getString("post_status")));
-            }
-            return posts;
-        });
-    }
-
-    /**
-     * Метод для получения поста по его ID.
-     *
-     * @param id Уникальный идентификатор поста
-     * @return Объект DataPost, соответствующий запрошенному посту, или null, если пост не найден
-     */
-    public DataPost getPostById(int id) {
-        String sql = "SELECT ID, post_title, post_content, post_status FROM wp_posts WHERE ID = ?";
-        return executeQuery(sql, rs -> {
-            if (rs.next()) {
-                DataPost.Title titleObj = new DataPost.Title(rs.getString("post_title"), "");
-                DataPost.Content contentObj = new DataPost.Content(rs.getString("post_content"), "");
-                return new DataPost(rs.getInt("ID"), titleObj, contentObj, rs.getString("post_status"));
-            }
-            return null;
-        }, id);
     }
 }
